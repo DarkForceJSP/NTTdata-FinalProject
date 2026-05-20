d7b8a2c0bdd48b20e68e4d5b7e8c779c7c65946d
Javier Sánchez Plata <101734589+DarkForceJSP@users.noreply.github.com>
2026-05-20 10:51:02 +0200

    Creacion del Backend con la API

--- Backend/cine/src/main/java/com/proyecto/cine/CineApplication.java
+++ Backend/cine/src/main/java/com/proyecto/cine/CineApplication.java
@@ -1,13 +1,13 @@
-package com.proyecto.peliculas;
+package com.proyecto.cine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PeliculasApplication {
+public class CineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PeliculasApplication.class, args);
+		SpringApplication.run(CineApplication.class, args);
 	}
 
 }
--- Backend/cine/src/test/java/com/proyecto/cine/CineApplicationTests.java
+++ Backend/cine/src/test/java/com/proyecto/cine/CineApplicationTests.java
@@ -1,10 +1,10 @@
-package com.proyecto.peliculas;
+package com.proyecto.cine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PeliculasApplicationTests {
+class CineApplicationTests {
 
 	@Test
 	void contextLoads() {
